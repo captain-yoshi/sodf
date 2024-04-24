fcfd735542249a9db905e0345866bc6f3c1437bd
Captain Yoshi <captain.yoshisaur@gmail.com>
2024-04-24 12:00:57 -0400

    [test] spherical cap volume

--- test/test_volume.cpp
+++ test/test_volume.cpp
@@ -4,7 +4,7 @@
 
 using namespace sodf::geometry;
 
-constexpr double VOLUME_EPSILON = 4 * std::numeric_limits<double>::epsilon();
+constexpr double VOLUME_EPSILON = 1e-09;
 
 TEST(RectangularPrismVolume, getVolume)
 {
@@ -17,6 +17,20 @@ TEST(RectangularPrismVolume, getVolume)
   EXPECT_NEAR(0.12, shape.getVolume(0.8), VOLUME_EPSILON);
 
   EXPECT_EQ(0.0, shape.getVolume(0.9));
+}
+
+TEST(SphericalCapVolume, getVolume)
+{
+  SphericalCapVolume shape(5.0, 5.0);
+
+  EXPECT_EQ(0.0, shape.getVolume(-0.5));
+  EXPECT_EQ(0.0, shape.getVolume(0.0));
+
+  EXPECT_NEAR(106.356001293, shape.getVolume(2.5), VOLUME_EPSILON);
+  EXPECT_NEAR(159.893976098, shape.getVolume(3.5), VOLUME_EPSILON);
+  EXPECT_NEAR(261.799387799, shape.getVolume(5.0), VOLUME_EPSILON);
+
+  EXPECT_EQ(0.0, shape.getVolume(5.0001));
 }
 
 int main(int argc, char** argv)
