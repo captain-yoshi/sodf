c0175153d36e049ce5e3798d8b21caf752652633
Captain Yoshi <captain.yoshisaur@gmail.com>
2024-04-29 21:21:25 -0400

    [impl] insertion element

---
+++ include/sodf/elements/insertion.h
@@ -0,0 +1,34 @@
+#ifndef INSERTION_H_
+#define INSERTION_H_
+
+#include <memory>
+#include <map>
+
+#include <sodf/geometry/transform.h>
+#include <sodf/element.h>
+
+namespace sodf {
+namespace elements {
+
+class Insertion : public Element
+{
+public:
+  Insertion(const geometry::Transform& insertion_frame, double pre_insertion_distance = 0.0);
+
+  virtual ~Insertion(){};
+
+  const geometry::Transform& tf() const;
+  double preInsertionDistance();
+
+  virtual bool addFramesToTree(KDL::Tree& tree) override;
+
+protected:
+  const geometry::Transform insertion_tf_;  // X axis pointing towards the bottom of the container, wrt. object origin
+
+  double pre_insertion_distance_;  // Along X axis wrt insertion_tf
+};
+
+}  // namespace elements
+}  // namespace sodf
+
+#endif  // INSERTION_H_
---
+++ src/elements/insertion.cpp
@@ -0,0 +1,27 @@
+#include <sodf/elements/insertion.h>
+
+namespace sodf {
+namespace elements {
+
+Insertion::Insertion(const geometry::Transform& insertion_tf, double pre_insertion_distance)
+  : insertion_tf_(insertion_tf), pre_insertion_distance_(pre_insertion_distance)
+{
+}
+
+bool Insertion::addFramesToTree(KDL::Tree& tree)
+{
+  return addFrameToTree(tree, insertion_tf_);
+}
+
+const geometry::Transform& Insertion::tf() const
+{
+  return insertion_tf_;
+}
+
+double Insertion::preInsertionDistance()
+{
+  return pre_insertion_distance_;
+}
+
+}  // namespace elements
+}  // namespace sodf
