965431bc3545b5c521acd3ab9501d6a8e758cc45
Captain Yoshi <captain.yoshisaur@gmail.com>
2024-05-14 15:50:59 -0400

    [impl] touchscreen state actions

--- include/sodf/elements/touchscreen.h
+++ include/sodf/elements/touchscreen.h
@@ -1,21 +1,35 @@
 #ifndef TOUCHSCREEN_H_
 #define TOUCHSCREEN_H_
 
+#include <functional>
+
+#include <sodf/behavior/state.h>
 #include <sodf/geometry/transform.h>
 #include <sodf/element.h>
 
 namespace sodf {
 namespace elements {
 
+using ActionElementMap = std::map<behavior::Action, ElementID>;
+
+using ActionsToElementIDCallback = std::function<std::string(behavior::Action)>;
+
 class Touchscreen : public Element
 {
 public:
-  Touchscreen(const geometry::Transform& center_tf, double size_y, double size_z);
+  Touchscreen(const geometry::Transform& center_tf, double size_y, double size_z, int current_state,
+              behavior::StateManager&& state_manager, ActionsToElementIDCallback callback);
 
   virtual ~Touchscreen(){};
 
   const geometry::Transform& centerTF() const;
   double pressForce() const;
+
+  int currentState() const;
+  void setCurrentState(int state);
+
+  std::vector<ElementID> computeActions(behavior::State start, behavior::State end,
+                                        const std::vector<behavior::Action>& end_actions, behavior::State& final_state);
 
   virtual const geometry::Transform* getTransform() const override
   {
@@ -29,7 +43,9 @@ protected:
   const double size_y_;
   const double size_z_;
 
-  std::string current_state_;
+  int current_state_;
+  behavior::StateManager state_manager_;
+  ActionsToElementIDCallback callback_;
 };
 
 }  // namespace elements
--- src/elements/touchscreen.cpp
+++ src/elements/touchscreen.cpp
@@ -3,14 +3,47 @@
 namespace sodf {
 namespace elements {
 
-Touchscreen::Touchscreen(const geometry::Transform& center_tf, double size_y, double size_z)
-  : center_tf_(center_tf), size_y_(size_y), size_z_(size_z), Element()
+Touchscreen::Touchscreen(const geometry::Transform& center_tf, double size_y, double size_z, int current_state,
+                         behavior::StateManager&& state_manager, ActionsToElementIDCallback callback)
+
+  : center_tf_(center_tf)
+  , size_y_(size_y)
+  , size_z_(size_z)
+  , current_state_(current_state)
+  , state_manager_(std::move(state_manager))
+  , callback_(callback)
+  , Element()
+
 {
 }
 
 const geometry::Transform& Touchscreen::centerTF() const
 {
   return center_tf_;
+}
+
+int Touchscreen::currentState() const
+{
+  return current_state_;
+}
+
+void Touchscreen::setCurrentState(int state)
+{
+  current_state_ = state;
+}
+
+std::vector<ElementID> Touchscreen::computeActions(behavior::State start, behavior::State end,
+                                                   const std::vector<behavior::Action>& end_actions,
+                                                   behavior::State& final_state)
+{
+  std::vector<behavior::Action> actions = state_manager_.computeActions(start, end, end_actions, final_state);
+
+  std::vector<ElementID> elements;
+
+  for (const auto& action : actions)
+    elements.push_back(callback_(action));
+
+  return elements;
 }
 
 }  // namespace elements
