8e61a9b656e92c768318733f90e8969974dc017d
Captain Yoshi <captain.yoshisaur@gmail.com>
2025-05-29 22:07:52 -0400

    Use generalized shapes: Line + SphericalSegment

--- include/sodf/components/shape.h
+++ include/sodf/components/shape.h
@@ -14,6 +14,7 @@ namespace components {
 enum class ShapeType
 {
   // 2D
+  Line,
   Rectangle,
   Circle,
   Polygon,
@@ -24,25 +25,27 @@ enum class ShapeType
   Cylinder,
   Sphere,
   Cone,
+  SphericalSegment,
   Plane,
   Mesh,
 };
 
 /*
-    | ShapeType | Dimensions                        | Axes                               | Vertices   |
-    |-----------|-----------------------------------|------------------------------------|------------|
-    | Rectangle | width, height                     | normal, width, height              |            |
-    | Circle    | radius                            | normal, major                      |            |
-    | Triangle  |                                   | normal, in-plane x, in-plane y     | 3 points   |
-    | Polygon   |                                   | normal, in-plane x, in-plane y     | N points   |
-    | Box       | width, height, depth              | x, y, z                            |            |
-    | Cylinder  | radius, height                    | symmetry, reference in base plane  |            |
-    | Sphere    | radius                            | (none)                             |            |
-    | Cone      | base_radius, top_radius*, height  | symmetry, reference in base plane  |            |
-    | Plane     | (optional: width, height)         | normal, in-plane x, in-plane y     |            |
-    | Mesh      | (none)                            | (none)                             | mesh_path  |
-
-    *top_radius = 0 for a perfect cone, > 0 for a frustum
+| ShapeType        | Dimensions                      | Axes                     | Vertices  | Description / Notes                                 |
+|------------------|---------------------------------|--------------------------|-----------|-----------------------------------------------------|
+| Line             | length (optional)               | direction                | 2 points  | Works for 2D or 3D, Z vertices = 0 for 2D           |
+|                  |                                 |                          |           | Bounded line if length > 0, infinte = 0             |
+| Rectangle        | width, height                   | normal, width, height    |           |                                                     |
+| Circle           | radius                          | normal, major            |           |                                                     |
+| Triangle         |                                 | normal, in-plane x-y     | 3 points  |                                                     |
+| Polygon          |                                 | normal, in-plane x-y     | N points  | Width, and height are wrt. the centroid             |
+| Box              | width, depth, height            | x, y, z                  |           |                                                     |
+| Cylinder         | radius, height                  | symmetry, ref base plane |           |                                                     |
+| Sphere           | radius                          | (none)                   |           |                                                     |
+| Cone             | base_radius, top_radius, height | symmetry, ref base plane |           | Frustum if top_radius > 0, cone if = 0              |
+| SphericalSegment | base_radius, top_radius, height | symmetry, ref base plane |           | Cap if one radius = 0, segment if both > 0          |
+| Plane            | width, height (optional)        | normal, in-plane x-y     |           | Bounded plane if width & height > 0, infinte if = 0 |
+| Mesh             | (none)                          | (none, defined in file)  | mesh_path |                                                     |
 */
 
 struct Shape
@@ -51,7 +54,7 @@ struct Shape
   std::vector<double> dimensions;
   std::vector<Eigen::Vector3d> axes;
   // Optional: additional data (e.g., list of points for polygons)
-  std::vector<Eigen::Vector2d> vertices_2d;  // For polygons, triangles in 2D plane
+  std::vector<Eigen::Vector3d> vertices;
   // Optional: mesh reference, etc.
   std::string mesh_path;
   Eigen::Vector3d scale = { 1.0, 1.0, 1.0 };
