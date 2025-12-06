aeeec8d2ac46cffab760a38eded9d5a2840df409
Captain Yoshi <captain.yoshisaur@gmail.com>
2025-12-06 14:22:06 -0500

    [fix] Seperate EntityID into own header
    Remove definition from origin constraint.

--- include/sodf/database/registry_p.h
+++ include/sodf/database/registry_p.h
@@ -7,25 +7,10 @@
 #include <string>
 
 #include <sodf/components/data_type.h>
+#include <sodf/database/entity_id.h>
 
 namespace sodf {
 namespace database {
-
-// Opaque entity id that SODF uses everywhere
-struct EntityID
-{
-  std::size_t index{ 0 };
-  std::size_t generation{ 0 };
-
-  bool operator==(const EntityID& o) const
-  {
-    return index == o.index && generation == o.generation;
-  }
-  bool operator!=(const EntityID& o) const
-  {
-    return !(*this == o);
-  }
-};
 
 // Forward-declare a backend tag type. You’ll implement specializations elsewhere.
 template <class Backend>
@@ -124,18 +109,3 @@ private:
 
 }  // namespace database
 }  // namespace sodf
-
-// Hash for EntityID so it can be used in unordered containers (diff/patch maps)
-namespace std {
-template <>
-struct hash<sodf::database::EntityID>
-{
-  std::size_t operator()(const sodf::database::EntityID& e) const noexcept
-  {
-    // A simple, stable combine
-    std::size_t h1 = std::hash<std::size_t>{}(e.index);
-    std::size_t h2 = std::hash<std::size_t>{}(e.generation);
-    return h1 ^ (h2 + 0x9e3779b97f4a7c15ULL + (h1 << 6) + (h1 >> 2));
-  }
-};
-}  // namespace std
