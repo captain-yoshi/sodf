3f20c9632c5724f22c663a3a976e98aeed9c4d27
Captain Yoshi <captain.yoshisaur@gmail.com>
2025-12-07 11:36:09 -0500

    [impl] make diff compatible with liquid level system

--- include/sodf/systems/liquid_level_joint.h
+++ include/sodf/systems/liquid_level_joint.h
@@ -1,23 +1,16 @@
-#ifndef SODF_SYSTEMS_LIQUID_LEVEL_JOINT_H_
-#define SODF_SYSTEMS_LIQUID_LEVEL_JOINT_H_
+#pragma once
 
+#include <Eigen/Core>
 #include <sodf/database/database.h>
-
-#include <sodf/components/container.h>
-#include <sodf/components/joint.h>
-#include <sodf/components/transform.h>
-#include <sodf/components/domain_shape.h>
-#include <sodf/components/shape.h>
 
 namespace sodf {
 namespace systems {
 
-/**
- * @brief Update all fluid liquid-level joints using the container's DomainShape.
- */
+// Existing base-db version
 void update_liquid_level_joints_from_domain(database::Database& db, Eigen::Vector3d& gravity_world);
+
+// NEW diff-safe version
+void update_liquid_level_joints_from_domain(database::DatabaseDiff& diff, Eigen::Vector3d& gravity_world);
 
 }  // namespace systems
 }  // namespace sodf
-
-#endif  // SODF_SYSTEMS_LIQUID_LEVEL_JOINT_H_
